459b3c0e1db388625e9abac4715ebca52481af07
Tooyama from Lab <hatsune_miku39@yeah.net>
2023-08-15 20:11:43 +0800

    2.15v

--- subsystem/cov/func_cov.sv
+++ subsystem/cov/func_cov.sv
@@ -42,6 +42,39 @@ class func_cov extends uvm_object;
         }
     endgroup: dualtimer_cg
 
+    covergroup uart0_cg with function sample(ahbl_transaction ahbl_tr);
+        option.per_instance = 1;
+
+        addr_check : coverpoint ahbl_tr.addr {
+            bins uart0data_offset = {`UART0_DATA_OFFSET};
+            bins uart0ctrl_offset = {`UART0_CTRL_OFFSET};
+            bins uart0intclear_offset = {`UART0_INTCLEAR_OFFSET};
+            bins uart0baud_offset = {`UART0_BAUDDIV_OFFSET};
+        }
+    endgroup: uart0_cg
+
+    covergroup uart1_cg with function sample(ahbl_transaction ahbl_tr);
+        option.per_instance = 1;
+
+        addr_check : coverpoint ahbl_tr.addr {
+            bins uart1data_offset = {`UART1_DATA_OFFSET};
+            bins uart1ctrl_offset = {`UART1_CTRL_OFFSET};
+            bins uart1intclear_offset = {`UART1_INTCLEAR_OFFSET};
+            bins uart1baud_offset = {`UART1_BAUDDIV_OFFSET};
+        }
+    endgroup: uart1_cg
+
+    covergroup uart2_cg with function sample(ahbl_transaction ahbl_tr);
+        option.per_instance = 1;
+
+        addr_check : coverpoint ahbl_tr.addr {
+            bins uart2data_offset = {`UART2_DATA_OFFSET};
+            bins uart2ctrl_offset = {`UART2_CTRL_OFFSET};
+            bins uart2intclear_offset = {`UART2_INTCLEAR_OFFSET};
+            bins uart2baud_offset = {`UART2_BAUDDIV_OFFSET};
+        }
+    endgroup: uart2_cg
+
     extern function new(string name="func_cov");
     extern function cg_sample(ref ahbl_transaction ahbl_tr);
 
@@ -56,14 +89,24 @@ function func_cov::new(string name="func_cov");
     timer0_cg = new();
     timer1_cg = new();
     dualtimer_cg = new();
+    uart0_cg = new();
+    uart1_cg = new();
+    uart2_cg = new();
 
     timer0_cg.set_inst_name({get_full_name(), ".timer0_cg"});
     timer1_cg.set_inst_name({get_full_name(), ".timer1_cg"});
     dualtimer_cg.set_inst_name({get_full_name(), ".dualtimer_cg"});
+    uart0_cg.set_inst_name({get_full_name(), ".uart0_cg"});
+    uart1_cg.set_inst_name({get_full_name(), ".uart1_cg"});
+    uart2_cg.set_inst_name({get_full_name(), ".uart2_cg"});
 endfunction: new
 
 function func_cov::cg_sample(ref ahbl_transaction ahbl_tr);
+    // ahbl_tr.print();
     timer0_cg.sample(ahbl_tr);
     timer1_cg.sample(ahbl_tr);
     dualtimer_cg.sample(ahbl_tr);
+    uart0_cg.sample(ahbl_tr);
+    uart1_cg.sample(ahbl_tr);
+    uart2_cg.sample(ahbl_tr);
 endfunction: cg_sample
